3adf7bbbb585ad35794ab1f7f7426d0c1c291295
dceoy <dnarsil+github@gmail.com>
2023-12-17 23:32:36 +0900

    Prettify codes using styler

--- src/clir.R
+++ src/clir.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
-'R Package Installer for Command Line Interface
+doc <- "\
+R Package Installer for Command Line Interface
 
 Usage:
     clir config [-v] [--quiet] [--init]
@@ -43,19 +44,19 @@ Commands:
 Arguments:
     <url>...            URLs of CRAN mirrors
     <repo>...           Drat repository names
-    <pkg>...            R package names' -> doc
+    <pkg>...            R package names"
 
-clir_version <- 'v1.2.0'
+clir_version <- "v1.2.0"
 
 fetch_clir_root <- function() {
   ca <- commandArgs(trailingOnly = FALSE)
-  fa <- ca[grepl('^--file=', ca)]
+  fa <- ca[grepl("^--file=", ca)]
   if (length(fa) == 1) {
-    f <- sub('--file=', '', fa)
+    f <- sub("--file=", "", fa)
     l <- Sys.readlink(f)
     if (is.na(l)) {
       cmd_path <- normalizePath(f)
-    } else if (startsWith(l, '/')) {
+    } else if (startsWith(l, "/")) {
       cmd_path <- normalizePath(l)
     } else {
       cmd_path <- normalizePath(file.path(dirname(f), l))
@@ -68,61 +69,72 @@ fetch_clir_root <- function() {
 
 main <- function(args, clir_root_dir = fetch_clir_root(),
                  r_lib = .libPaths()[1]) {
-  ncpus <- ifelse(is.null(args[['--cpus']]),
-                  parallel::detectCores(), as.integer(args[['--cpus']]))
-  options(warn = 1, Ncpus = ncpus, verbose = args[['-v']])
-  if (args[['-v']]) {
-    print(list(warn = 1, Ncpus = ncpus, verbose = args[['-v']]))
+  ncpus <- ifelse(is.null(args[["--cpus"]]),
+    parallel::detectCores(), as.integer(args[["--cpus"]])
+  )
+  options(warn = 1, Ncpus = ncpus, verbose = args[["-v"]])
+  if (args[["-v"]]) {
+    print(list(warn = 1, Ncpus = ncpus, verbose = args[["-v"]]))
   }
-  loaded <- list(args = args,
-                 pkg = sapply(c('devtools', 'drat', 'stringr', 'yaml'),
-                              require, character.only = TRUE,
-                              quietly = (! args[['-v']])),
-                 src = source(file.path(clir_root_dir, 'src/util.R')))
+  loaded <- list(
+    args = args,
+    pkg = sapply(c("devtools", "drat", "stringr", "yaml"),
+      require,
+      character.only = TRUE,
+      quietly = (!args[["-v"]])
+    ),
+    src = source(file.path(clir_root_dir, "src/util.R"))
+  )
   make_clir_dirs(clir_root_dir = clir_root_dir)
-  clir_yml <- file.path(clir_root_dir, 'r/clir.yml')
-  repos <- load_repos(clir_yml = clir_yml, quiet = args[['--quiet']])
+  clir_yml <- file.path(clir_root_dir, "r/clir.yml")
+  repos <- load_repos(clir_yml = clir_yml, quiet = args[["--quiet"]])
   options(repos = repos)
-  if (args[['-v']]) {
-    print(c(loaded, sapply(c('repos'), getOption)))
+  if (args[["-v"]]) {
+    print(c(loaded, sapply(c("repos"), getOption)))
   }
-  if (args[['config']]) {
-    print_config(clir_yml = clir_yml, r_lib = r_lib, init = args[['--init']])
-  } else if (args[['cran']]) {
-    if (args[['--list']]) {
+  if (args[["config"]]) {
+    print_config(clir_yml = clir_yml, r_lib = r_lib, init = args[["--init"]])
+  } else if (args[["cran"]]) {
+    if (args[["--list"]]) {
       print_cran_mirrors(https = TRUE)
-    } else if (length(args[['<url>']]) > 0) {
-      add_config(new = args[['<url>']], key = 'cran_urls', clir_yml = clir_yml)
+    } else if (length(args[["<url>"]]) > 0) {
+      add_config(new = args[["<url>"]], key = "cran_urls", clir_yml = clir_yml)
     } else {
-      stop('URLs or --list must be passed for this command.')
+      stop("URLs or --list must be passed for this command.")
     }
-  } else if (args[['drat']]) {
-    add_config(new = args[['<repo>']], key = 'drat_repos', clir_yml = clir_yml)
-  } else if (args[['update']]) {
-    update_pkgs(repos = repos, r_lib = r_lib, bioc = args[['--bioc']],
-                quiet = args[['--quiet']])
-  } else if (args[['install']]) {
-    install_pkgs(pkgs = args[['<pkg>']], repos = repos,
-                 bioc = args[['--bioc']], devt = args[['--devt']],
-                 r_lib = r_lib, upgrade = (! args[['--no-upgrade']]),
-                 quiet = args[['--quiet']])
-  } else if (args[['download']]) {
-    download.packages(pkgs = args[['<pkg>']], destdir = args[['--dest-dir']],
-                      repos = repos, type = 'source')
-  } else if (args[['uninstall']]) {
-    remove.packages(pkgs = args[['<pkg>']], lib = r_lib)
-  } else if (args[['validate']]) {
-    validate_loading(pkgs = args[['<pkg>']], quiet = args[['--quiet']])
-  } else if (args[['session']]) {
-    print_sessions(pkgs = args[['<pkg>']])
+  } else if (args[["drat"]]) {
+    add_config(new = args[["<repo>"]], key = "drat_repos", clir_yml = clir_yml)
+  } else if (args[["update"]]) {
+    update_pkgs(
+      repos = repos, r_lib = r_lib, bioc = args[["--bioc"]],
+      quiet = args[["--quiet"]]
+    )
+  } else if (args[["install"]]) {
+    install_pkgs(
+      pkgs = args[["<pkg>"]], repos = repos,
+      bioc = args[["--bioc"]], devt = args[["--devt"]],
+      r_lib = r_lib, upgrade = (!args[["--no-upgrade"]]),
+      quiet = args[["--quiet"]]
+    )
+  } else if (args[["download"]]) {
+    download.packages(
+      pkgs = args[["<pkg>"]], destdir = args[["--dest-dir"]],
+      repos = repos, type = "source"
+    )
+  } else if (args[["uninstall"]]) {
+    remove.packages(pkgs = args[["<pkg>"]], lib = r_lib)
+  } else if (args[["validate"]]) {
+    validate_loading(pkgs = args[["<pkg>"]], quiet = args[["--quiet"]])
+  } else if (args[["session"]]) {
+    print_sessions(pkgs = args[["<pkg>"]])
   } else {
-    stop('invalid subcommand')
+    stop("invalid subcommand")
   }
 }
 
-if (! interactive()) {
+if (!interactive()) {
   args <- docopt::docopt(doc, version = clir_version)
-  if (args[['--quiet']]) {
+  if (args[["--quiet"]]) {
     suppressMessages(main(args = args))
   } else {
     main(args = args)
--- src/util.R
+++ src/util.R
@@ -1,11 +1,15 @@
 #!/usr/bin/env Rscript
 
-default_config <- list(cran_urls = c('https://cran.rstudio.com/'),
-                       drat_repos = c('eddelbuettel'))
+default_config <- list(
+  cran_urls = c("https://cran.rstudio.com/"),
+  drat_repos = c("eddelbuettel")
+)
 
 make_clir_dirs <- function(clir_root_dir, exist_ok = TRUE) {
-  sapply(file.path(clir_root_dir, c('r', 'r/library')),
-         dir.create, showWarnings = (! exist_ok))
+  sapply(file.path(clir_root_dir, c("r", "r/library")),
+    dir.create,
+    showWarnings = (!exist_ok)
+  )
 }
 
 load_repos <- function(clir_yml, quiet = FALSE) {
@@ -14,25 +18,25 @@ load_repos <- function(clir_yml, quiet = FALSE) {
   } else {
     cf <- default_config$cran_urls
   }
-  if (require('drat', quietly = TRUE) && ('drat_repos' %in% names(cf))) {
+  if (require("drat", quietly = TRUE) && ("drat_repos" %in% names(cf))) {
     drat:::addRepo(account = cf$drat_repos)
   }
-  repos <- getOption('repos')
-  if ('cran_urls' %in% names(cf)) {
+  repos <- getOption("repos")
+  if ("cran_urls" %in% names(cf)) {
     cran <- cf$cran_urls[1]
-  } else if (repos == "@CRAN@")  {
+  } else if (repos == "@CRAN@") {
     cran <- default_config$cran_urls[1]
   } else {
-    cran <- repos['CRAN']
+    cran <- repos["CRAN"]
   }
-  return(c(CLAN = cran, repos[names(repos) != 'CRAN']))
+  return(c(CLAN = cran, repos[names(repos) != "CRAN"]))
 }
 
 print_config <- function(clir_yml, r_lib = .libPaths()[1], init = FALSE) {
   if (init) {
     cf <- default_config
     yaml::write_yaml(cf, file = clir_yml)
-    message(stringr::str_c('Initialized: ', clir_yml))
+    message(stringr::str_c("Initialized: ", clir_yml))
   } else if (file.exists(clir_yml)) {
     cf <- yaml::read_yaml(clir_yml)
   } else {
@@ -49,32 +53,38 @@ add_config <- function(new, key, clir_yml) {
     cf <- default_config
   }
   yaml::write_yaml(cf, file = clir_yml)
-  message(stringr::str_c('Updated: ', clir_yml))
+  message(stringr::str_c("Updated: ", clir_yml))
 }
 
 print_cran_mirrors <- function(https = TRUE) {
   d <- data.frame(getCRANmirrors())
   if (https) {
-    print(subset(d, grepl('^https://', d$URL))[, c('Name', 'URL')])
+    print(subset(d, grepl("^https://", d$URL))[, c("Name", "URL")])
   } else {
-    print(d[, c('Name', 'URL')])
+    print(d[, c("Name", "URL")])
   }
 }
 
 load_n_run_bioclite <- function(pkgs, repos, r_lib = .libPaths()[1], ...) {
-  BiocManager::install(pkgs = pkgs, lib.loc = r_lib, lib = r_lib,
-                       siteRepos = repos, update = TRUE, ask = FALSE, ...)
+  BiocManager::install(
+    pkgs = pkgs, lib.loc = r_lib, lib = r_lib,
+    siteRepos = repos, update = TRUE, ask = FALSE, ...
+  )
 }
 
 update_pkgs <- function(repos, bioc = FALSE, r_lib = .libPaths()[1],
                         check_built = TRUE, quiet = FALSE) {
   if (bioc) {
-    load_n_run_bioclite(pkgs = rownames(installed.packages(lib.loc = r_lib)),
-                        repos = repos, r_lib = r_lib, checkBuilt = check_built,
-                        quiet = quiet)
+    load_n_run_bioclite(
+      pkgs = rownames(installed.packages(lib.loc = r_lib)),
+      repos = repos, r_lib = r_lib, checkBuilt = check_built,
+      quiet = quiet
+    )
   } else {
-    update.packages(lib.loc = r_lib, repos = repos, ask = FALSE,
-                    checkBuilt = check_built, quiet = quiet)
+    update.packages(
+      lib.loc = r_lib, repos = repos, ask = FALSE,
+      checkBuilt = check_built, quiet = quiet
+    )
   }
 }
 
@@ -83,85 +93,101 @@ install_pkgs <- function(pkgs, repos, devt, bioc = FALSE,
                          upgrade = TRUE, depend = TRUE, quiet = FALSE,
                          clean = TRUE) {
   installed_pkgs <- rownames(installed.packages(lib.loc = r_lib))
-  ps <- list(all = pkgs,
-             old = intersect(pkgs, installed_pkgs),
-             new = setdiff(pkgs, installed_pkgs))
+  ps <- list(
+    all = pkgs,
+    old = intersect(pkgs, installed_pkgs),
+    new = setdiff(pkgs, installed_pkgs)
+  )
   if (upgrade || (length(ps$new) > 0)) {
     if (is.null(devt)) {
       if (bioc) {
-        load_n_run_bioclite(pkgs = ps$all, repos = repos, r_lib = r_lib,
-                            checkBuilt = check_built, quiet = quiet)
+        load_n_run_bioclite(
+          pkgs = ps$all, repos = repos, r_lib = r_lib,
+          checkBuilt = check_built, quiet = quiet
+        )
       } else {
         if (upgrade && (length(ps$old) > 0)) {
-          update.packages(instPkgs = ps$old, repos = repos, lib.loc = r_lib,
-                          ask = FALSE, checkBuilt = check_built, quiet = quiet)
+          update.packages(
+            instPkgs = ps$old, repos = repos, lib.loc = r_lib,
+            ask = FALSE, checkBuilt = check_built, quiet = quiet
+          )
         }
         if (length(ps$new) > 0) {
-          install.packages(pkgs = ps$new, repos = repos, lib = r_lib,
-                           dependencies = depend, quiet = quiet, clean = clean)
+          install.packages(
+            pkgs = ps$new, repos = repos, lib = r_lib,
+            dependencies = depend, quiet = quiet, clean = clean
+          )
         }
       }
-    } else if (devt %in% c('cran', 'github', 'bitbucket', 'bioc')) {
-      if (! require('devtools', quietly = TRUE)) {
-        install.packages(pkgs = 'devtools', repos = repos, lib = r_lib,
-                         dependencies = TRUE, quiet = quiet, clean = clean)
+    } else if (devt %in% c("cran", "github", "bitbucket", "bioc")) {
+      if (!require("devtools", quietly = TRUE)) {
+        install.packages(
+          pkgs = "devtools", repos = repos, lib = r_lib,
+          dependencies = TRUE, quiet = quiet, clean = clean
+        )
       }
-      if (require('devtools', quietly = TRUE)) {
+      if (require("devtools", quietly = TRUE)) {
         if (upgrade) {
           targets <- ps$all
         } else {
           targets <- ps$new
         }
         f <- switch(devt,
-                    'cran' = devtools::install_cran,
-                    'github' = devtools::install_github,
-                    'bitbucket' = devtools::install_bitbucket,
-                    'bioc' = devtools::install_bioc)
-        withr::with_libpaths(r_lib,
-                             f(targets, dependencies = depend, quiet = quiet))
+          "cran" = devtools::install_cran,
+          "github" = devtools::install_github,
+          "bitbucket" = devtools::install_bitbucket,
+          "bioc" = devtools::install_bioc
+        )
+        withr::with_libpaths(
+          r_lib,
+          f(targets, dependencies = depend, quiet = quiet)
+        )
       } else {
-        stop('Loading of devtools failed.')
+        stop("Loading of devtools failed.")
       }
     } else {
-      stop('Invalid installation type.')
+      stop("Invalid installation type.")
     }
   } else {
-    message('The packages already installed.')
+    message("The packages already installed.")
   }
 }
 
 validate_loading <- function(pkgs, quiet = FALSE) {
   v <- sapply(as.vector(pkgs), require, character.only = TRUE)
-  result <- list(succeeded = names(v)[v], failed = names(v)[! v])
-  if (! quiet) {
-    if (require('devtools', quietly = TRUE)) {
+  result <- list(succeeded = names(v)[v], failed = names(v)[!v])
+  if (!quiet) {
+    if (require("devtools", quietly = TRUE)) {
       print(devtools::session_info(pkgs = pkgs, include_base = TRUE))
     }
-    cat('\n- Loading test ', stringr::str_c(rep('-', 63), collapse = ''), '\n',
-        sep = '')
+    cat("\n- Loading test ", stringr::str_c(rep("-", 63), collapse = ""), "\n",
+      sep = ""
+    )
     if (length(result$succeeded) > 0) {
-      cat(' Succeeded:\n  - ',
-          stringr::str_c(result$succeeded, collapse = '\n  - '), '\n',
-          sep = '')
+      cat(" Succeeded:\n  - ",
+        stringr::str_c(result$succeeded, collapse = "\n  - "), "\n",
+        sep = ""
+      )
     }
     if (length(result$failed) > 0) {
-      cat(' Failed:\n  - ',
-          stringr::str_c(result$failed, collapse = '\n  - '), '\n',
-          sep = '')
+      cat(" Failed:\n  - ",
+        stringr::str_c(result$failed, collapse = "\n  - "), "\n",
+        sep = ""
+      )
     }
-    cat('\n')
+    cat("\n")
     if (length(result$failed) == 0) {
-      message('Loading succeeded.')
+      message("Loading succeeded.")
     }
   }
   if (length(result$failed) > 0) {
-    stop('Loading failed.')
+    stop("Loading failed.")
   }
 }
 
 print_sessions <- function(pkgs) {
-  if (! require('devtools', quietly = TRUE)) {
-    stop('This command requires devtools.')
+  if (!require("devtools", quietly = TRUE)) {
+    stop("This command requires devtools.")
   } else if (length(pkgs) > 0) {
     devtools::session_info(pkgs = pkgs, include_base = TRUE)
   } else {
